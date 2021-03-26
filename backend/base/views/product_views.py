7c47c929a3944aeb9098f6fb31e026bc383be843
Azhar Hussain Khan <azhuss77@gmail.com>
2021-03-27 00:42:45 +0530

    added paginator

--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from base.models import Product, Review
 from base.serializer import ProductSerializer
@@ -17,9 +18,26 @@ def getProducts(request):
 
     products = Product.objects.filter(name__icontains=query)
 
+    page = request.query_params.get('page')
+    paginator = Paginator(products, 4)
+
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+
+    if page == None:
+        page = 1
+    page = int(page)
+
     #many here represents that we expect multiple products in return
     serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
+    return Response({
+        "products" : serializer.data,
+        "page" : page,
+        "pages" : paginator.num_pages})
 
 
 @api_view(['GET'])
